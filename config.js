3b1c7e06ee46a7005998ed708b5d42317bc553f9
AX9T-hack <oulaisales@gmail.com>
2025-07-27 22:36:53 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",  
+SESSION_ID: process.env.SESSION_ID || "ALI-MD~WMFzwChS#0ZriUohxDsN3E81RNhYxigpuCwytDkl0rI8b6gVNFQk",  
 //chatbot integration 
 CHATBOT: process.env.CHATBOT || "on",
 // add your Session Id 
